84f0ecafbc028df5db3075575a31ae27263f4279
idandamri <idandamri@gmail.com>
2016-03-10 15:20:28 +0200

    created card layout + card visual item
    RealItem + rec_view.. + erased FAB

---
+++ app/src/main/java/com/example/idan/second_project/RealItem.java
@@ -0,0 +1,26 @@
+package com.example.idan.second_project;
+
+/**
+ * Created by idan on 09/03/2016.
+ */
+public class RealItem {
+
+    private int player;
+    private String name;
+
+    public int getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(int player) {
+        this.player = player;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
